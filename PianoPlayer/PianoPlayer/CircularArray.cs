506f3776ad1100ce23f752ae3bbf16c87905daab
Francesca <francesca.pinci1@gmail.com>
2020-02-10 18:05:19 -0800

    second commit, francesca

---
+++ PianoPlayer/PianoPlayer/CircularArray.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace PianoPlayer
+{
+	public class CircularArray: IRingBuffer
+	{
+		private double[] CircleArray;
+		
+		public CircularArray(int ArrLength)
+		{
+			CircleArray = new double[ArrLength];
+		}
+
+		public double this[int index] => throw new NotImplementedException();
+
+		public int Length => throw new NotImplementedException();
+
+		public void Fill(double[] array)
+		{
+			for (int i = 0; i < CircleArray.Length; i++)
+			{
+				array[i] = CircleArray[i];
+			}
+			throw new NotImplementedException();
+		}
+
+		public double Shift(double value)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
--- PianoPlayer/PianoPlayer/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace PianoPlayer
-{
-    public class Class1
-    {
-    }
-}
