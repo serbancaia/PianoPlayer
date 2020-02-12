0e3eb77de863a9dd67ddfeee75db0e1381e77946
Alin Serban Caia <c4i4.4lin@gmail.com>
2020-02-12 15:54:02 -0500

    I created the PianoWire.cs class, made it implement the IMusicalInstrument interface, wrote its constructor which takes frequency and samplingRate as its input to assign values to the class' private fields called frequency and samplingRate, wrote, inside PianoWire.cs, both methods that were implemented from the IMusicalInstrument interface and made them throw a NotImplementException

---
+++ PianoPlayer/PianoPlayer/PianoWire.cs
@@ -0,0 +1,21 @@
+﻿namespace PianoPlayer
+{
+	public class PianoWire : IMusicalInstrument
+	{
+		private int frequency;
+		private int samplingRate;
+		public PianoWire(int frequency, int samplingRate)
+		{
+			this.frequency = frequency;
+			this.samplingRate = samplingRate;
+		}
+		public double Sample(double decay)
+		{
+			throw new System.NotImplementedException();
+		}
+		public void Strike() 
+		{
+			throw new System.NotImplementedException();
+		}
+	}
+}
