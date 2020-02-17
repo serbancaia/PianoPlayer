13be539c6afb43d2072ab77169b807ccb36430f4
Alin Serban Caia <c4i4.4lin@gmail.com>
2020-02-17 00:46:35 -0500

    I fixed Piano class: I made the StrikeKey method call the Strike method in the PianoWire class on a key, I fixed the syntax of the statement in the CharacterKey method, I fixed the += sign inside the Play method. I also made test project called PianoTests inside our solution, I added a test class called AudioTest and built the header for the method which should test the constructor inside the Audio class

---
+++ PianoPlayer/PianoTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("PianoTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PianoTests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4490b36c-a149-4a5c-9daa-7e03a777b13d")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
