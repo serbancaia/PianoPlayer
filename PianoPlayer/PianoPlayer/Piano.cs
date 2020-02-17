1cf3f15feef1fdf0174b7ad9ea5f182df1141034
Francesca <francesca.pinci1@gmail.com>
2020-02-16 20:11:50 -0500

    Test project added and updated classes

--- PianoPlayer/PianoPlayer/Piano.cs
+++ PianoPlayer/PianoPlayer/Piano.cs
@@ -7,24 +7,32 @@ namespace PianoPlayer
 		private int samplingRate;
 		private List<IMusicalInstrument> wireList;
 
+		/// <summary> 
+        /// This constructor takes 2 params to create a Piano object
+        /// </summary>
+        /// <param name="keys">String of keys</param>
+        /// <param name="samplingRate">Smpling rate value</param>
 		public Piano(string keys = "q2we4r5ty7u8i9op-[=zxdcfvgbnjmk,.;/' ", int samplingRate = 44100)
 		{
 			this.keys = keys;
 			this.samplingRate = samplingRate;
 		}
 
-		//associates a char in the keys string with a note and its frequency
+		/// <summary>
+        /// This method associates a char in the keys string with a note and its frequency.
+        /// </summary>
 		public void CharacterKey()
-		{
-			
+		{	
 			foreach (char c in keys.ToCharArray())
 			{
 				wireList.Add(new PianoWire(Math.Pow(2, (keys.IndexOf(c) - 24) / 12) * 440), samplingRate);
 			}
 		}
 
-		//strikes piano key corresponding to the specified character
-		//if it returns -1, a message will be displayed
+		/// <summary> This method strikes the piano key corresponding to the specified character.
+		/// If it returns -1, a message will be displayed
+        /// </summary>
+		/// <param name="key"> Char in the string </param>
 		public void StrikeKey(char key)
 		{
 			if (keys.IndexOf(key) == -1)
@@ -33,16 +41,18 @@ namespace PianoPlayer
 			}
 		}
 
-		//goes through piano wires and returns sum of their sample
+		/// <summary>
+        /// This method goes through piano wires and returns sum of their sample
+		/// </summary>
+        /// <returns>Sum of the samples</returns>
 		public double Play()
 		{ 
 			double sum = 0;
-			foreach (int i in wireList)
+			foreach (var i in wireList)
 			{
-				sum += wireList.get(i);
+				sum =+ i.Sample(0.996);
 			}
 			return sum;
 		}
 	}
-	
 }
---
+++ PianoTests/CircularArrayTest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PianoTests
+{
+    class CircularArrayTest
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
